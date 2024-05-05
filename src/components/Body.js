d37ca9c7ea65017b7d3571ccf1e45752d97ee968
M1roel <157500795+M1roel@users.noreply.github.com>
2024-05-05 16:36:58 +0200

    Alle Dateien hochgeladen

---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+function Body() {
+    return (
+      <body className="Body">
+      <img src="/images/bandfoto_01.jpg" alt="Logo" className="Bandfoto2" />
+      </body>
+    ); 
+}
+
+export default Body;
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    // Event Listener zum Überwachen des Scroll-Events hinzufügen
+    window.addEventListener('scroll', handleScroll);
+
+    // Event Listener beim Komponentenabbau entfernen
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    // Überprüfe, ob der Benutzer nach unten gescrollt hat
+    if (window.scrollY > 0) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  return (
+    <div className={isScrolled ? 'Navbar Navbar--scrolled' : 'Navbar'}>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/merge">Merge</a></li>
+                <li><a href="/tourdaten">Tourdaten</a></li>
+                <li><a href="/kontakt">Kontakt</a></li>
+                <li><a href="/über-uns">Über uns</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
