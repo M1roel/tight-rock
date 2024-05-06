8d145a995f100299275c2bd6f4bff9b3e4780b2c
M1roel <157500795+M1roel@users.noreply.github.com>
2024-05-06 15:17:26 +0200

    App.jsx: keine Änderung, Body.js: Bandbild entfernt

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Body from './components/Body';
-import Counter from './components/Counter';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Navbar />
       <Header />
       <Body />
-      <Counter />
     </div>
   );
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,6 @@
 function Body() {
     return (
       <body className="Body">
-      <img src="/images/bandfoto_01.jpg" alt="Logo" className="Bandfoto2" />
       </body>
     ); 
 }
