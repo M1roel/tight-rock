ee72aee5610160ddae5df6e85d81912a49e64ebd
M1roel <ppfautsch@yahoo.de>
2024-05-08 08:56:32 +0200

    Titel Bild geändert, Old Enlish Font, Einfliegender Text, seperate Header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import '../Header.css'; // Stelle sicher, dass die CSS-Datei importiert ist
+
 function Header() {
   return (
     <div className="App-header">
-      <img src="/images/bandfoto_02.png" alt="Logo" className="Bandfoto" />
+      <div className="Image-container">
+        <img src="/images/bandfoto_03_clear.jpg" alt="Logo" className="Bandfoto" />
+        <div className="Text-overlay">
+          <h1 className="Tight">Tight</h1>
+          <h2 className="FemaleFronted">Female Fronted Heavy-Rock</h2>
+        </div>
+      </div>
     </div>
   );
 }
