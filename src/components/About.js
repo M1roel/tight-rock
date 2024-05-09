381974420a29a1f6a7a4c01f2fb582702be77064
M1roel <157500795+M1roel@users.noreply.github.com>
2024-05-09 12:06:05 +0200

    Start der About Seite + dazugehöriges CSS

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,24 @@
+import "../About.css";
+import "../App.css";
+
 function About() {
-    return (
-        <div className="About-main">
-            
-        </div>
-    );    
+  return (
+    <div className="About-main">
+      <div className="Kerstin">
+        <img
+          src="/images/707b4778-b856-4706-8676-4da89eba4b59.png"
+          alt="saengerin tight kerstin"
+        ></img>
+        <h2>Kerstin Pfautsch - Vocals</h2>
+        <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
+        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
+        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.  
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
