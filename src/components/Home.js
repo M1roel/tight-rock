b1b58b6c1ba092fa2dfe8d0893c0ef75bee4ed73
M1roel <157500795+M1roel@users.noreply.github.com>
2024-05-09 08:50:19 +0200

    Home.js angepasst

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
+import Header from "./Header";
+import Body from "./Body";
+
 function Home() {
     return (
         <div className="Home">
-            
+            <Header />
+            <Body />
         </div>
     );
 }
