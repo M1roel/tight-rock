cd85bd3c43997cb2a3a1592ffc7856bfe78555ef
M1roel <ppfautsch@yahoo.de>
2024-05-10 08:44:03 +0200

    Header grundlegend geändert

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,13 @@ function Header() {
   return (
     <div className="App-header">
       <div className="Image-container">
-        <img src="/images/bandfoto_03_clear.jpg" alt="Logo" className="Bandfoto" />
-        <div className="Text-overlay">
+        <img src="/images/2fc80658-d8c1-41be-9071-8001f3f76a44.png" alt="Logo" className="Bandfoto" />
+        <div className="Schriftzug">
           <h1 className="Tight">Tight</h1>
           <h2 className="FemaleFronted">Female Fronted Heavy-Rock</h2>
         </div>
       </div>
+      <div className="Header-bottom-bar"></div>
     </div>
   );
 }
