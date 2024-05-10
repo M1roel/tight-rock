bad404dc7a316a094d80ddf5081276f1362cd7da
M1roel <157500795+M1roel@users.noreply.github.com>
2024-05-10 14:22:01 +0200

    Klasse beschreibung_1 und beschreibung_2 zu beschreibung_links und beschreibung_rechts geändert

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,8 @@ function About() {
   return (
     <div className="About-main">
       <div className="About-kerstin">
-        <img
-          src="/images/about-kerstin.png"
-          alt="saengerin tight kerstin"
-        ></img>
-        <div className="beschreibung_1">
+        <img src="/images/about-kerstin.png" alt="saengerin tight kerstin"></img>
+        <div className="beschreibung_links">
           <h2>Kerstin Pfautsch - Vocals</h2>
           <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
@@ -22,7 +19,7 @@ function About() {
       </div>
       <div className="About-stephan">
         <img src="/images/about-stephan.png" alt="guitar tight stephan"></img>
-        <div className="beschreibung_2">
+        <div className="beschreibung_rechts">
           <h2>Stephan Georg - Guitar</h2>
           <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
@@ -35,7 +32,7 @@ function About() {
       </div>
       <div className="About-bernd">
         <img src="/images/about-bernd.png" alt="drums tight bernd"></img>
-        <div className="beschreibung_1">
+        <div className="beschreibung_links">
           <h2>Bernd - Drums</h2>
           <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
@@ -48,7 +45,7 @@ function About() {
       </div>
       <div className="About-joerg">
         <img src="/images/about-joerg.png" alt="bass tight joerg"></img>
-        <div className="beschreibung_2">
+        <div className="beschreibung_rechts">
           <h2>Jörg - Bass</h2>
           <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
@@ -61,7 +58,7 @@ function About() {
       </div>
       <div className="About-matthias">
         <img src="/images/about-matthias.png" alt="guitar tight matthias"></img>
-        <div className="beschreibung_1">
+        <div className="beschreibung_links">
           <h2>Matthias - Guitar</h2>
           <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
