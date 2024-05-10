6baf4dcb30428c7e4861a7b35e1d21195c24398c
M1roel <157500795+M1roel@users.noreply.github.com>
2024-05-10 14:12:49 +0200

    About Seite - CSS Flexbox - Bilder und Texte hinzugefügt

--- src/components/About.js
+++ src/components/About.js
@@ -4,18 +4,73 @@ import "../styles/App.css";
 function About() {
   return (
     <div className="About-main">
-      <div className="Kerstin">
+      <div className="About-kerstin">
         <img
-          src="/images/707b4778-b856-4706-8676-4da89eba4b59.png"
+          src="/images/about-kerstin.png"
           alt="saengerin tight kerstin"
         ></img>
-        <h2>Kerstin Pfautsch - Vocals</h2>
-        <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
-        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
-        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.  
-        </p>
+        <div className="beschreibung_1">
+          <h2>Kerstin Pfautsch - Vocals</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+      <div className="About-stephan">
+        <img src="/images/about-stephan.png" alt="guitar tight stephan"></img>
+        <div className="beschreibung_2">
+          <h2>Stephan Georg - Guitar</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+      <div className="About-bernd">
+        <img src="/images/about-bernd.png" alt="drums tight bernd"></img>
+        <div className="beschreibung_1">
+          <h2>Bernd - Drums</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+      <div className="About-joerg">
+        <img src="/images/about-joerg.png" alt="bass tight joerg"></img>
+        <div className="beschreibung_2">
+          <h2>Jörg - Bass</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+      <div className="About-matthias">
+        <img src="/images/about-matthias.png" alt="guitar tight matthias"></img>
+        <div className="beschreibung_1">
+          <h2>Matthias - Guitar</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet.
+          </p>
+        </div>
       </div>
     </div>
   );
