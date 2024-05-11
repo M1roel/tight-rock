1d4b7ad6faa852d21f0b8a8b7d5cd1a83a51916b
M1roel <ppfautsch@yahoo.de>
2024-05-11 20:31:49 +0200

    Kontakt Seite begonnen

--- src/components/Kontakt.js
+++ src/components/Kontakt.js
@@ -1,20 +1,22 @@
 import Form from 'react-bootstrap/Form';
 
 function Kontakt() {
-  return (
-    <div className="Kontakt-main">
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
-        </Form.Group>
-      </Form>
-    </div>
-  );
-}
-
-export default Kontakt;
+    return (
+      <div className="Kontakt-container">
+        <div className="Kontakt-main">
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Deine eMail Adresse</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Deine Nachricht an uns</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Kontakt;
