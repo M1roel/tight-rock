41fddf0383636be61d29ef374b7039752b70010c
M1roel <ppfautsch@yahoo.de>
2024-05-22 13:03:56 +0200

    Tourdaten und Texte für die Bandmitglieder hinzugefügt

--- src/components/About.js
+++ src/components/About.js
@@ -4,71 +4,86 @@ import "../styles/About.css";
 import "../styles/App.css";
 
 function About() {
-
   useEffect(() => {
     // Nach dem Rendern das Bild von Kerstin animieren
-    const kerstinImage = document.querySelector('.About-kerstin img');
-    kerstinImage.classList.add('animate-from-right');
+    const kerstinImage = document.querySelector(".About-kerstin img");
+    kerstinImage.classList.add("animate-from-right");
 
-    const kerstinAbout = document.querySelector('.beschreibung_links_kerstin');
-    kerstinAbout.classList.add('animate-from-left');
+    const kerstinAbout = document.querySelector(".beschreibung_links_kerstin");
+    kerstinAbout.classList.add("animate-from-left");
 
     // Scroll-Handler hinzufügen
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     // Cleanup-Funktion für den Effekt
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
+
   const handleScroll = () => {
-    const rightElements = document.querySelectorAll('.About-mathias img'); // Hier Bilder einfügen
+    const rightElements = document.querySelectorAll(".About-mathias img"); // Hier Bilder einfügen
     rightElements.forEach((element) => {
-      const elementPosition = element.getBoundingClientRect().top + (element.offsetHeight / 2); // Position des Bildes relativ zur Mitte des Bildes berechnen
-      if (elementPosition < window.innerHeight && elementPosition > 0) { // Überprüfen, ob das Bild zur Hälfte im sichtbaren Bereich des Browserfensters liegt
-        element.classList.add('animate-from-right');
+      const elementPosition =
+        element.getBoundingClientRect().top + element.offsetHeight / 2; // Position des Bildes relativ zur Mitte des Bildes berechnen
+      if (elementPosition < window.innerHeight && elementPosition > 0) {
+        // Überprüfen, ob das Bild zur Hälfte im sichtbaren Bereich des Browserfensters liegt
+        element.classList.add("animate-from-right");
       }
     });
 
-    const leftElements = document.querySelectorAll('.About-stephan img, .About-joerg img'); // Hier Bilder einfügen
+    const leftElements = document.querySelectorAll(
+      ".About-stephan img, .About-joerg img"
+    ); // Hier Bilder einfügen
     leftElements.forEach((element) => {
-      const elementPosition = element.getBoundingClientRect().top + (element.offsetHeight / 2); // Position des Bildes relativ zur Mitte des Bildes berechnen
-      if (elementPosition < window.innerHeight && elementPosition > 0) { // Überprüfen, ob das Bild zur Hälfte im sichtbaren Bereich des Browserfensters liegt
-        element.classList.add('animate-from-left');
+      const elementPosition =
+        element.getBoundingClientRect().top + element.offsetHeight / 2; // Position des Bildes relativ zur Mitte des Bildes berechnen
+      if (elementPosition < window.innerHeight && elementPosition > 0) {
+        // Überprüfen, ob das Bild zur Hälfte im sichtbaren Bereich des Browserfensters liegt
+        element.classList.add("animate-from-left");
       }
     });
 
-    const beschreibungLinksElements = document.querySelectorAll('.beschreibung_links');
+    const beschreibungLinksElements = document.querySelectorAll(
+      ".beschreibung_links"
+    );
     beschreibungLinksElements.forEach((element) => {
-      const elementPosition = element.getBoundingClientRect().top + (element.offsetHeight / 2);
+      const elementPosition =
+        element.getBoundingClientRect().top + element.offsetHeight / 2;
       if (elementPosition < window.innerHeight && elementPosition > 0) {
-        element.classList.add('animate-from-left');
+        element.classList.add("animate-from-left");
       }
     });
 
-    const beschreibungRechtsElements = document.querySelectorAll('.beschreibung_rechts');
+    const beschreibungRechtsElements = document.querySelectorAll(
+      ".beschreibung_rechts"
+    );
     beschreibungRechtsElements.forEach((element) => {
-      const elementPosition = element.getBoundingClientRect().top + (element.offsetHeight / 2);
+      const elementPosition =
+        element.getBoundingClientRect().top + element.offsetHeight / 2;
       if (elementPosition < window.innerHeight && elementPosition > 0) {
-        element.classList.add('animate-from-right');
+        element.classList.add("animate-from-right");
       }
     });
-    
   };
- 
+
   return (
     <div className="About-main">
       <div className="About-kerstin">
-        <img src="/images/about-kerstin.png" alt="saengerin tight kerstin"></img>
+        <img
+          src="/images/about-kerstin.png"
+          alt="saengerin tight kerstin"
+        ></img>
         <div className="beschreibung_links_kerstin">
           <h2>Kerstin - Vocals</h2>
           <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet.
+            Kerstin ist die charismatische Sängerin der Rockband Tight, die mit
+            ihrer kraftvollen Stimme und dynamischen Bühnenpräsenz die Herzen
+            der Fans erobert. Seit ihrer Kindheit von Musik fasziniert, bringt
+            Kerstin ihre Leidenschaft mit Tight auf die Bühne und begeistert das
+            Publikum mit energiegeladenen Auftritten. Mit ihrer Liebe zum Rock
+            und ihrer beeindruckenden Performance sorgt sie dafür, dass jedes
+            Konzert ein unvergessliches Erlebnis wird.
           </p>
         </div>
       </div>
@@ -77,11 +92,17 @@ function About() {
         <div className="beschreibung_rechts">
           <h2>Stephan - Guitar</h2>
           <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet.
+            Stephan ist der talentierte Solo-Gitarrist, Bandgründer und
+            Songwriter der Rockband Tight. Mit seinem virtuosen Gitarrenspiel
+            und kreativen Songwriting prägt er den einzigartigen Sound der Band.
+            Stephan hat Tight ins Leben gerufen, um seine Leidenschaft für
+            Rockmusik zu teilen und seine musikalischen Visionen zu
+            verwirklichen. Als Songwriter ist Stephan das kreative Herz von
+            Tight. Seine Kompositionen zeichnen sich durch eingängige Riffs,
+            kraftvolle Melodien und tiefgründige Texte aus, die die Hörer in
+            ihren Bann ziehen. Auf der Bühne überzeugt er durch seine technische
+            Brillanz und seine energiegeladene Performance, die jedes Konzert zu
+            einem besonderen Erlebnis macht.
           </p>
         </div>
       </div>
@@ -103,11 +124,17 @@ function About() {
         <div className="beschreibung_rechts">
           <h2>Jörg - Bass</h2>
           <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet.
+            Jörg ist der erfahrene Bassist von Tight, der mit seinem markanten
+            Spiel und seiner tiefen Verbundenheit zur Musik das Fundament der
+            Band bildet. Seine präzisen Basslinien verleihen den Songs eine
+            kraftvolle und zugleich melodische Dimension, die den Sound von
+            Tight einzigartig macht. Jörg ist nicht nur ein Meister seines
+            Instruments, sondern auch ein wichtiger Teil des Zusammenhalts in
+            der Band. Seine ruhige und bodenständige Art trägt dazu bei, dass
+            Tight nicht nur musikalisch, sondern auch menschlich eine
+            harmonische Einheit bildet. Mit seinem einfühlsamen Spiel und seiner
+            Leidenschaft für die Musik bereichert Jörg jede Tight-Performance
+            und lässt das Publikum in den rhythmischen Groove eintauchen.
           </p>
         </div>
       </div>
@@ -116,11 +143,15 @@ function About() {
         <div className="beschreibung_links">
           <h2>M.D. - Guitar</h2>
           <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet.
+            M.D, der Lead-Gitarrist von Tight, fasziniert auf der Bühne mit
+            seiner einzigartigen Ausstrahlung. Seine Sonnenbrille und der Hut
+            verleihen ihm einen coolen Look, der den Stil der Band
+            unterstreicht. Sein Gitarrenspiel ist voller Feuer und Leidenschaft,
+            und sein Charisma zieht das Publikum in seinen Bann. M.D ist nicht
+            nur ein begnadeter Musiker, sondern auch eine beeindruckende
+            Persönlichkeit auf der Bühne. Seine Performance verleiht den
+            Tight-Konzerten eine unverwechselbare Atmosphäre, die Fans für immer
+            in Erinnerung bleiben wird.
           </p>
         </div>
       </div>
--- src/components/Tourdaten.js
+++ src/components/Tourdaten.js
@@ -5,8 +5,8 @@ import "../styles/Tourdaten.css";
 function Tourdaten() {
   return (
     <div className="Tourdaten-main">
-      <div className="Tourdaten-left">
-        <div className="Rock-sommer-nacht-text">
+      <div className="Tourdaten-block">
+        <div className="Tourdaten-text">
           <h2>Rock Sommer Nacht 2024, Grevenbroich</h2>
           <p>
             Die Rock Sommer Nacht in Grevenbroich ist mehr als nur ein
@@ -16,9 +16,48 @@ function Tourdaten() {
             nur Herzen zum Tanzen bringt, sondern auch Gutes in unserer
             Gemeinschaft bewirkt.
           </p>
+          <span>Am 07.06.2024 findet ihr uns ab 19.00 Uhr auf der Bühne</span>
         </div>
-        <div className="Rock-sommer-nacht-bild">
-            <img src="/images/rock-sommer-nacht-2024.jpg"></img>
+        <div className="Tourdaten-bild">
+          <img src="/images/rock-sommer-nacht-2024.jpg"></img>
+        </div>
+      </div>
+
+      <div className="Tourdaten-block">
+        <div className="Tourdaten-text">
+          <h2>Herdecke, Rockstarter Open Air</h2>
+          <p>
+            Das Herdecker Rockstarter Open Air ist ein bekanntes Musikfestival,
+            das jährlich in Herdecke, Nordrhein-Westfalen, stattfindet. Es
+            bietet eine Plattform für lokale und überregionale Rockbands, um
+            ihre Musik einem breiten Publikum zu präsentieren. Das Festival
+            zeichnet sich durch seine entspannte Atmosphäre und die
+            Unterstützung von Nachwuchskünstlern aus. Neben der Musik gibt es
+            oft auch ein Rahmenprogramm mit verschiedenen Aktivitäten und
+            Verpflegungsständen, die das Event zu einem beliebten Treffpunkt für
+            Musikliebhaber machen.
+          </p>
+        </div>
+        <div className="Tourdaten-bild">
+          <img src="/images/rockstarter.png"></img>
+        </div>
+      </div>
+
+      <div className="Tourdaten-block">
+        <div className="Tourdaten-text">
+          <h2>Herdecke, Rockstarter Open Air</h2>
+          <p>
+            Das Fuchsfestival in Krefeld, Uerdingen, ist ein mit Spannung
+            erwartetes Musikfestival, das am 20. Juli 2024 stattfindet. Dieses
+            Event wird in einer gemütlichen Kneipe am Rhein ausgerichtet, die
+            für ihre entspannte Atmosphäre und ihre abwechslungsreichen
+            Veranstaltungen bekannt ist. Die Kneipe in Uerdingen bietet das
+            ganze Jahr über zahlreiche Events und Live-Musik-Abende an, bei
+            denen Musikliebhaber auf ihre Kosten kommen.
+          </p>
+        </div>
+        <div className="Tourdaten-bild">
+          <img src="/images/fuchs-festival.png"></img>
         </div>
       </div>
     </div>
