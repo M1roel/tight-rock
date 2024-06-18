2375dc0242c089584762baebd0e5e950c70c291a
M1roel <ppfautsch@yahoo.de>
2024-06-18 18:58:36 +0200

    Bilder hinzugefuegt und Karusell angepasst

--- src/components/ControlledCarousel.js
+++ src/components/ControlledCarousel.js
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+// ControlledCarousel.js
+
+import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import bandfoto_01 from './bandfoto_01.jpg';
+import images from './img';
 
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
@@ -11,29 +13,19 @@ function ControlledCarousel() {
 
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-      <img src={bandfoto_01} alt="First slide" className="d-block w-100" />
-        {/* <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={bandfoto_01} alt="Second slide" className="d-block w-100" />
-        {/* <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src={bandfoto_01} alt="Third slide" className="d-block w-100" />
-        {/* <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
+      {images.map((imgSrc, idx) => (
+        <Carousel.Item key={idx}>
+          <img
+            src={imgSrc}
+            alt={`Slide ${idx + 1}`}
+            className="d-block w-100"
+          />
+          {/* <Carousel.Caption>
+            <h3>Slide label {idx + 1}</h3>
+            <p>Optional slide description.</p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 }
