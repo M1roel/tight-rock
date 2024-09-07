0f01f94496fbdd9826c46ace6dacb45ba8697e28
M1roel <ppfautsch@yahoo.de>
2024-09-07 11:23:47 +0200

    change header picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="App-header">
       <div className="Image-container">
-        <img src="/images/bandfoto_03_clear.jpg" alt="Logo" className="Bandfoto" />
+        <img src="/images/headerimg.jpg" alt="Logo" className="Bandfoto" />
         <div className="Text-overlay">
           <h1 className="Tight">Tight</h1>
           <h2 className="FemaleFronted">Female Fronted Heavy-Rock</h2>
